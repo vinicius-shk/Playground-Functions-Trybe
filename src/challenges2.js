6cc62cc467d5097b2643e8c6c6277503015e7b13
Vinicius Oliveira <vinicius.shk@gmail.com>
2022-04-12 17:02:42 -0300

    Resolução do requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,8 +1,33 @@
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(array) {
+  let telefone = "";
+  function repeticao(array, numero) {
+    let contador = 0;
+    for(let key in array) {
+      if(array[key] === numero) {
+        contador += 1;
+      }
+    }
+  return contador;
+  }
+  if(array.length !== 11) {
+    return "Array com tamanho incorreto.";
+  }
+  for(let key in array) {
+    if(repeticao(array, array[key]) >= 3 || array[key] < 0 || array[key] > 9) {
+      return "não é possível gerar um número de telefone com esses valores";
+    } else if(key === '0') {
+      telefone += "(" + array[key];
+    } else if(key === '1') {
+      telefone += array[key] + ") ";
+    } else if(key === '6') {
+      telefone += array[key] + "-";
+    } else {
+      telefone += array[key];
+    }
+  }
+return telefone;
 }
-
 // Desafio 12
 function triangleCheck() {
   // seu código aqui
